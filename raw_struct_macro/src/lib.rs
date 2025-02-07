1c27530f8e864505f02dadd6ba5d353047527a31
yohei naruse <yohei.naruse@600.jp>
2025-02-08 02:27:01 +0900

    fix

--- raw_struct_macro/src/lib.rs
+++ raw_struct_macro/src/lib.rs
@@ -1,16 +1,50 @@
 use proc_macro::TokenStream;
 use quote::quote;
-use syn::{parse_macro_input, DeriveInput, Data, Fields, Type, PathArguments, GenericArgument};
+use syn::{parse_macro_input, DeriveInput, Data, Fields, Type, PathArguments, GenericArgument, Meta, NestedMeta, Lit, Attribute};
 
-#[proc_macro_derive(RawStruct)]
+// 文字列バリデーション用の設定を保持する構造体
+struct StringValidation {
+    min_length: Option<usize>,
+    max_length: Option<usize>,
+}
+
+// 属性からバリデーション設定を解析する関数
+fn parse_string_validation(attrs: &[Attribute]) -> StringValidation {
+    let mut validation = StringValidation {
+        min_length: None,
+        max_length: None,
+    };
+
+    for attr in attrs {
+        if attr.path.is_ident("validate") {
+            if let Ok(Meta::List(meta_list)) = attr.parse_meta() {
+                for nested in meta_list.nested.iter() {
+                    if let NestedMeta::Meta(Meta::NameValue(name_value)) = nested {
+                        if name_value.path.is_ident("min_length") {
+                            if let Lit::Int(lit) = &name_value.lit {
+                                validation.min_length = lit.base10_parse().ok();
+                            }
+                        } else if name_value.path.is_ident("max_length") {
+                            if let Lit::Int(lit) = &name_value.lit {
+                                validation.max_length = lit.base10_parse().ok();
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    validation
+}
+
+#[proc_macro_derive(RawStruct, attributes(validate))]
 pub fn raw_struct_derive(input: TokenStream) -> TokenStream {
-    // 入力を解析
     let input = parse_macro_input!(input as DeriveInput);
     let struct_name = input.ident;
     let raw_struct_name = format!("Raw{}", struct_name);
     let raw_struct_ident = syn::Ident::new(&raw_struct_name, struct_name.span());
 
-    // フィールドの処理
     let fields = match input.data {
         Data::Struct(ref data) => {
             match data.fields {
@@ -21,39 +55,39 @@ pub fn raw_struct_derive(input: TokenStream) -> TokenStream {
         _ => panic!("RawStruct は構造体のみサポートします"),
     };
 
-    // 新しいフィールドの生成
+    // すべてのフィールドをOption<String>として生成
     let raw_fields = fields.iter().map(|f| {
         let field_name = &f.ident;
-        let field_type = &f.ty;
-        
-        // Option<T> の場合の処理
-        let new_type = if is_option_type(field_type) {
-            quote! { Option<String> }
-        } else {
-            quote! { String }
-        };
-
         quote! {
-            #field_name: #new_type
+            #field_name: Option<String>
         }
     });
 
-    // バリデーション用のフィールド名とその型の取得
-    let validation_fields = fields.iter().map(|f| {
+    let validation_checks = fields.iter().map(|f| {
         let field_name = &f.ident;
-        let field_type = if is_option_type(&f.ty) {
-            get_inner_type(&f.ty).unwrap()
+        let field_type = &f.ty;
+        let validation = parse_string_validation(&f.attrs);
+        let is_optional = is_option_type(field_type);
+        let base_type = if is_optional {
+            get_inner_type(field_type).unwrap()
         } else {
-            &f.ty
+            field_type
         };
-        (field_name, field_type)
-    });
-
-    // バリデーションロジックの生成
-    let validation_checks = validation_fields.map(|(field_name, field_type)| {
-        let type_str = quote!(#field_type).to_string();
         
-        match type_str.as_str() {
+        let required_check = if !is_optional {
+            quote! {
+                if self.#field_name.is_none() {
+                    let mut err = validator::ValidationError::new(stringify!(#field_name));
+                    err.message = Some(format!("フィールド '{}' は必須項目です", stringify!(#field_name)).into());
+                    errors.add(stringify!(#field_name), err);
+                }
+            }
+        } else {
+            quote! {}
+        };
+
+        let type_str = quote!(#base_type).to_string();
+        let value_check = match type_str.as_str() {
             "u8" | "u16" | "u32" | "u64" | "u128" => quote! {
                 if let Some(value) = &self.#field_name {
                     match value {
@@ -62,11 +96,11 @@ pub fn raw_struct_derive(input: TokenStream) -> TokenStream {
                             err.message = Some(format!("フィールド '{}' に負の値 ({}) が指定されましたが、{}型は負の値を受け付けません", 
                                 stringify!(#field_name), 
                                 value,
-                                stringify!(#field_type)
+                                stringify!(#base_type)
                             ).into());
                             errors.add(stringify!(#field_name), err);
                         },
-                        v => match v.parse::<#field_type>() {
+                        v => match v.parse::<#base_type>() {
                             Ok(_) => {},
                             Err(e) => {
                                 let mut err = validator::ValidationError::new(stringify!(#field_name));
@@ -78,8 +112,8 @@ pub fn raw_struct_derive(input: TokenStream) -> TokenStream {
                                     false => format!("フィールド '{}' の値 ({}) が {}型の範囲（0 ～ {}) を超えています", 
                                         stringify!(#field_name),
                                         value,
-                                        stringify!(#field_type),
-                                        #field_type::MAX
+                                        stringify!(#base_type),
+                                        #base_type::MAX
                                     )
                                 }.into());
                                 errors.add(stringify!(#field_name), err);
@@ -90,7 +124,7 @@ pub fn raw_struct_derive(input: TokenStream) -> TokenStream {
             },
             "i8" | "i16" | "i32" | "i64" | "i128" => quote! {
                 if let Some(value) = &self.#field_name {
-                    match value.parse::<#field_type>() {
+                    match value.parse::<#base_type>() {
                         Ok(_) => {},
                         Err(e) => {
                             let mut err = validator::ValidationError::new(stringify!(#field_name));
@@ -102,9 +136,9 @@ pub fn raw_struct_derive(input: TokenStream) -> TokenStream {
                                 false => format!("フィールド '{}' の値 ({}) が {}型の範囲（{} ～ {}) を超えています", 
                                     stringify!(#field_name),
                                     value,
-                                    stringify!(#field_type),
-                                    #field_type::MIN,
-                                    #field_type::MAX
+                                    stringify!(#base_type),
+                                    #base_type::MIN,
+                                    #base_type::MAX
                                 )
                             }.into());
                             errors.add(stringify!(#field_name), err);
@@ -114,7 +148,7 @@ pub fn raw_struct_derive(input: TokenStream) -> TokenStream {
             },
             "f32" | "f64" => quote! {
                 if let Some(value) = &self.#field_name {
-                    match value.parse::<#field_type>() {
+                    match value.parse::<#base_type>() {
                         Ok(_) => {},
                         Err(_) => {
                             let mut err = validator::ValidationError::new(stringify!(#field_name));
@@ -142,29 +176,78 @@ pub fn raw_struct_derive(input: TokenStream) -> TokenStream {
                     }
                 }
             },
-            "String" => quote! {
-                // String型の場合は常に有効
+            "String" => {
+                let length_validation = match (validation.min_length, validation.max_length) {
+                    (Some(min), Some(max)) => quote! {
+                        let len = value.chars().count();
+                        if len < #min || len > #max {
+                            let mut err = validator::ValidationError::new(stringify!(#field_name));
+                            err.message = Some(format!("フィールド '{}' の文字数が制限（{} ～ {} 文字）を超えています（現在: {} 文字）", 
+                                stringify!(#field_name),
+                                #min,
+                                #max,
+                                len
+                            ).into());
+                            errors.add(stringify!(#field_name), err);
+                        }
+                    },
+                    (Some(min), None) => quote! {
+                        let len = value.chars().count();
+                        if len < #min {
+                            let mut err = validator::ValidationError::new(stringify!(#field_name));
+                            err.message = Some(format!("フィールド '{}' の文字数が最小文字数（{} 文字）未満です（現在: {} 文字）", 
+                                stringify!(#field_name),
+                                #min,
+                                len
+                            ).into());
+                            errors.add(stringify!(#field_name), err);
+                        }
+                    },
+                    (None, Some(max)) => quote! {
+                        let len = value.chars().count();
+                        if len > #max {
+                            let mut err = validator::ValidationError::new(stringify!(#field_name));
+                            err.message = Some(format!("フィールド '{}' の文字数が最大文字数（{} 文字）を超えています（現在: {} 文字）", 
+                                stringify!(#field_name),
+                                #max,
+                                len
+                            ).into());
+                            errors.add(stringify!(#field_name), err);
+                        }
+                    },
+                    (None, None) => quote! {}
+                };
+
+                quote! {
+                    if let Some(value) = &self.#field_name {
+                        #length_validation
+                    }
+                }
             },
             _ => quote! {
                 if let Some(value) = &self.#field_name {
-                    match value.parse::<#field_type>() {
+                    match value.parse::<#base_type>() {
                         Ok(_) => {},
                         Err(_) => {
                             let mut err = validator::ValidationError::new(stringify!(#field_name));
                             err.message = Some(format!("フィールド '{}' の値 ({}) が {}型として無効です", 
                                 stringify!(#field_name),
                                 value,
-                                stringify!(#field_type)
+                                stringify!(#base_type)
                             ).into());
                             errors.add(stringify!(#field_name), err);
                         }
                     }
                 }
             }
+        };
+
+        quote! {
+            #required_check
+            #value_check
         }
     });
 
-    // 新しい構造体の生成（Deserializeトレイトを追加し、Validateを実装する）
     let expanded = quote! {
         #[derive(Debug, Clone, serde::Deserialize)]
         pub struct #raw_struct_ident {
--- src/main.rs
+++ src/main.rs
@@ -5,14 +5,16 @@ use raw_struct_macro::RawStruct;
 pub struct Record {
     pub a: Option<u16>,
     pub b: Option<i32>,
-    pub c: Option<String>,
+    #[validate(min_length = 5)]
+    pub c: String,
+    #[validate(min_length = 5)]
     pub d: Option<String>,
 }
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     let csv_data = "\
 a,b,c,d
-43434532432432432,20,foo,bar
+43434532432432432,20,foo,
 1,30,baz,qux
 -3232,40,hello,world
 ";
@@ -24,6 +26,7 @@ a,b,c,d
         if let Err(errors) = raw.validate() {
             println!("バリデーションエラー: {:?}", errors);
         }
+        println!("======")
     }
     Ok(())
 }
